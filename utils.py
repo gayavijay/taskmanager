bdbfa14ce8f9eb2f1a429b14aa4b609df9f25e52
Gayathri Vijay <gayathritharavijay@gmail.com>
2025-04-25 15:28:24 -0700

    date formatting helper function for priority toggles

--- utils.py
+++ utils.py
@@ -1,1 +1,7 @@
-print("Test")
+from datetime import datetime
+
+def parse_date(date_str):
+    try:
+        return datetime.strptime(date_str, "%Y-%m-%d").date()
+    except ValueError:
+        return None
