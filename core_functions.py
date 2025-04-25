58816070a566b42d76f80c2df393a6f1afdf60a6
Gayathri Vijay <gayathritharavijay@gmail.com>
2025-04-25 15:27:19 -0700

    main functions of task manager defined

--- core_functions.py
+++ core_functions.py
@@ -1,1 +1,14 @@
-print("Hello")
+from database import add_task, get_all_tasks, delete_task, mark_task_complete
+
+class TaskManager:
+    def add(self, title, due_date, priority, category):
+        add_task(title, due_date, priority, category)
+
+    def get_tasks(self):
+        return get_all_tasks()
+
+    def complete(self, task_id):
+        mark_task_complete(task_id)
+
+    def delete(self, task_id):
+        delete_task(task_id)
